70ec8c1b4f6d632826a9df4cccfb3f3dd5dfbb05
Kieran <kemackle98@gmail.com>
2021-08-26 09:20:31 +1000

    docs: optimisation running

--- runfile.py
+++ runfile.py
@@ -5,5 +5,10 @@ at.verbosity = 1
 at.add_strategy('long_ema_crossover')
 at.backtest(start = '1/1/2020',
             end = '1/1/2021')
+# at.optimise(opt_params=['slow_ema', 'fast_ema'],
+#             bounds=[(10, 50), (5, 30)])
 at.show_plot = True
 at.run()
+
+# Demo Strategies:
+# long_ema_crossover
