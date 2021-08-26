c1a643960a16d1db943bd4f5735da222414928cb
Kieran <kemackle98@gmail.com>
2021-08-27 08:54:12 +1000

    docs: updated runfile for macd example

--- runfile.py
+++ runfile.py
@@ -2,13 +2,15 @@ from autotrader.autotrader import AutoTrader
 
 at = AutoTrader()
 at.verbosity = 1
-at.add_strategy('long_ema_crossover')
+at.add_strategy('macd')
 at.backtest(start = '1/1/2020',
-            end = '1/1/2021')
-# at.optimise(opt_params=['slow_ema', 'fast_ema'],
-#             bounds=[(10, 50), (5, 30)])
+            end = '1/1/2021',
+            leverage = 30)
+# at.optimise(opt_params=['MACD_fast', 'MACD_slow'],
+#             bounds=[(5, 20), (20, 40)])
 at.show_plot = True
 at.run()
+
 
 # Demo Strategies:
 # long_ema_crossover
