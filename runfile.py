5a4dbd64e139d2b5fc348d9f908a498c8675e146
Kieran <kemackle98@gmail.com>
2021-11-05 14:55:28 +1000

    docs: added rebalance tolerance param

--- runfile.py
+++ runfile.py
@@ -9,9 +9,8 @@ from autotrader.autotrader import AutoTrader
 
 at = AutoTrader()
 at.configure(show_plot=True)
-at.add_strategy('long_ema_crossover')
+at.add_strategy('rebalance')
 at.backtest(start = '1/1/2020',
             end = '1/8/2020',
-            leverage = 30,
-            initial_balance=1000)
+            initial_balance=10000)
 at.run()
--- strategies/portfolio_rebalance.py
+++ strategies/portfolio_rebalance.py
@@ -20,26 +20,51 @@ class Rebalance:
     def generate_signal(self, i, current_position):
         ''' Define strategy to determine entry signals '''
         
-        signal_dict = {'order_type': 'market'}
+        signal_dict = {'order_type': 'market',
+                       'direction': 0}
         
         # Get current positions held by account
         rebalance_instruments = list(self.params['rebalance_percentages'].keys())
         current_holdings = self.broker.get_open_positions(rebalance_instruments)
         
-        if len(current_holdings) == 0:
-            # Initialise portfolio
+        if self.instrument in current_holdings:
+            print('yehaw')
+            
+            asset_allocation = {}
+            total_value = 0
+            for instrument in rebalance_instruments:
+                position_value = current_holdings[instrument]['total_margin']
+                asset_allocation[instrument] = position_value
+                total_value += position_value
+            
+            allocation_error = abs(100*asset_allocation[self.instrument]/total_value - \
+                self.params['rebalance_percentages'][self.instrument])
+            
+            if allocation_error > self.params['rebalance_tolerance']:
+                # Rebalance
+                # calculate required size to meet balance percentage
+                # calculate difference between required size and current position size
+                # Place order using calculated size
+                size = self.calculate_position_size()
+            
+        else:
             signal_dict['direction'] = 1 # buy
             signal_dict['size'] = self.calculate_position_size()
         
-        else:
-            # Portfolio has holdings
+        # if len(current_holdings) == 0:
+        #     # Initialise portfolio
+        #     signal_dict['direction'] = 1 # buy
+        #     signal_dict['size'] = self.calculate_position_size()
+        
+        # else:
+        #     # Portfolio has holdings
             
-            # Check if it is time to rebalance yet
-            self.params['rebalance_interval']
+        #     # Check if it is time to rebalance yet
+        #     self.params['rebalance_interval']
             
-            # work out percentage of each holding 
-            # calculate new amount (ie. how much to sell or buy) of self.instrument
-            # Let other bots take care of their own instruments
+        #     # work out percentage of each holding 
+        #     # calculate new amount (ie. how much to sell or buy) of self.instrument
+        #     # Let other bots take care of their own instruments
         
         return signal_dict
     
