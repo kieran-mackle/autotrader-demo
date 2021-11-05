530e4bdfd1eb6d874423dda5338a0c94faf20aec
Kieran <kemackle98@gmail.com>
2021-11-05 14:32:35 +1000

    docs: basic size calculator implemented

--- strategies/portfolio_rebalance.py
+++ strategies/portfolio_rebalance.py
@@ -16,14 +16,29 @@ class Rebalance:
     def generate_signal(self, i, current_position):
         ''' Define strategy to determine entry signals '''
         
+        signal_dict = {}
+        
         # Get current positions held by account
         rebalance_instruments = list(self.params['rebalance_percentages'].keys())
         current_holdings = self.broker.get_open_positions(rebalance_instruments)
+        
+        if len(current_holdings) == 0:
+            # Initialise portfolio
+            
+            
         
         
         # First check if the instrument is being held yet, if not, buy
         # will need to consider the price of all others to calculate the 
         # size
+        if self.instrument in current_holdings:
+            # Holding instrument already
+            print("already")
+        
+        else:
+            # Instrument isn't held yet, buy
+            signal_dict['direction'] = 1
+            signal_dict['size'] = self.calculate_position_size()
         
         
         # work out percentage of each holding 
@@ -32,7 +47,7 @@ class Rebalance:
         
         
         # order_type  = 'market'
-        signal_dict = {}
+       
         
         return signal_dict
     
@@ -41,4 +56,11 @@ class Rebalance:
         Calculates position size of instrument based on desired position
         value and current price per unit.
         '''
+        
+        account_balance = self.broker.get_balance()
+        instrument_allocation_pc = self.params['rebalance_percentages'][self.instrument] / 100
+        instrument_allocation_value = instrument_allocation_pc * account_balance
+        position_size = instrument_allocation_value / self.data.Close[-1]
+        
+        return position_size
     
