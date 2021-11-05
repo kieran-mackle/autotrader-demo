f63410303a7118be8bbc32b8fe718eaa8cf1b4af
Kieran <kemackle98@gmail.com>
2021-11-05 14:24:06 +1000

    docs: building skeleton

--- strategies/portfolio_rebalance.py
+++ strategies/portfolio_rebalance.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 class Rebalance:
     '''
     Portfolio rebalancer.
@@ -14,24 +13,32 @@ class Rebalance:
         self.instrument = instrument
         self.broker = broker
         
-        
     def generate_signal(self, i, current_position):
         ''' Define strategy to determine entry signals '''
         
         # Get current positions held by account
+        rebalance_instruments = list(self.params['rebalance_percentages'].keys())
+        current_holdings = self.broker.get_open_positions(rebalance_instruments)
+        
+        
+        # First check if the instrument is being held yet, if not, buy
+        # will need to consider the price of all others to calculate the 
+        # size
         
         
         # work out percentage of each holding 
         # calculate new amount (ie. how much to sell or buy) of self.instrument
         # Let other bots take care of their own instruments
         
-        # First check if the instrument is being held yet, if not, buy
-        # will need to consider the price of all others to calculate the 
-        # size
         
         # order_type  = 'market'
         signal_dict = {}
         
         return signal_dict
-
+    
+    def calculate_position_size(self):
+        ''' 
+        Calculates position size of instrument based on desired position
+        value and current price per unit.
+        '''
     
