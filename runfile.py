d5f176aae93cb7145789af35ff048dc5a8339583
Kieran <kemackle98@gmail.com>
2021-11-05 12:34:26 +1000

    docs: updated for clarity

--- runfile.py
+++ runfile.py
@@ -1,14 +1,17 @@
 '''
 AutoTrader Demo Runfile.
+------------------------
+To run a strategy, simply specify the name of the strategy's configuration
+filename prefix in the "add_strategy()" method on line 12.
 '''
 
 from autotrader.autotrader import AutoTrader
 
 at = AutoTrader()
-at.verbosity = 1
-at.add_strategy('MTF_ema_crossover')
+at.configure(show_plot=True)
+at.add_strategy('macd')
 at.backtest(start = '1/1/2020',
             end = '1/8/2020',
-            leverage = 30)
-at.show_plot = True
+            leverage = 30,
+            initial_balance=1000)
 at.run()
