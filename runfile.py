96268c3a2ea6a932e61820f35b5c1fa7e6ccc1ae
Kieran Mackle <kemackle98@gmail.com>
2021-11-06 15:20:05 +1000

    docs: reverted to MACD demo

--- runfile.py
+++ runfile.py
@@ -8,9 +8,10 @@ filename prefix in the "add_strategy()" method on line 12.
 from autotrader.autotrader import AutoTrader
 
 at = AutoTrader()
-at.configure(show_plot=True)
-at.add_strategy('rebalance')
+at.configure(show_plot=True, verbosity=1)
+at.add_strategy('macd')
 at.backtest(start = '1/1/2020',
-            end = '1/8/2020',
-            initial_balance=10000)
+            end = '1/10/2021',
+            initial_balance=1000,
+            leverage = 20)
 at.run()
