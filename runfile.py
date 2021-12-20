0a2ddde035996e535cd0b307f1c3ae05c09fe5d2
Kieran Mackle <kemackle98@gmail.com>
2021-12-20 20:52:32 +1000

    docs: added sma momentum strategy (#3)

--- runfile.py
+++ runfile.py
@@ -9,9 +9,11 @@ from autotrader.autotrader import AutoTrader
 
 at = AutoTrader()
 at.configure(show_plot=True, verbosity=1)
-at.add_strategy('macd')
-at.backtest(start = '1/1/2020',
-            end = '1/1/2021',
+at.add_strategy('sma_momentum')
+at.backtest(start = '10/12/2021',
+            end = '19/12/2021',
             initial_balance=1000,
             leverage = 30)
+at.optimise(opt_params=['sma_period'],
+            bounds=[(20, 60)])
 at.run()
