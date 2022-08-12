781e1381beef5391e1ea6eae7edbbc46ee6cab78
Kieran <kemackle98@gmail.com>
2022-08-12 15:35:56 +1000

    docs: updated runfile

--- runfile.py
+++ runfile.py
@@ -1,22 +1,10 @@
-'''
-AutoTrader Demo Runfile
------------------------
-To run a strategy, simply specify the name of the strategy's configuration
-filename prefix in the "add_strategy()" method on line 12.
-'''
+from autotrader import AutoTrader
 
-from autotrader.autotrader import AutoTrader
-
-# Create AutoTrader instance, configure is, and run backtest
+# Create AutoTrader instance, configure it, and run backtest
 at = AutoTrader()
-at.configure(verbosity=1, show_plot=True)
+at.configure(verbosity=1, show_plot=True, feed='yahoo', mode='periodic')
 at.add_strategy('macd')
-at.backtest(start = '1/1/2022',
-            end = '1/5/2022',
-            initial_balance=1000,
-            leverage = 30)
+at.backtest(start = '1/1/2022', end = '1/5/2022')
+at.virtual_account_config(initial_balance=1000, leverage = 30)
 at.run()
-
-# Extract trading bot(s)
-bot = at.get_bots_deployed()
 
