867216322e6d726bab138b3bef4fa18d81663bc1
Kieran <kemackle98@gmail.com>
2022-10-28 14:41:05 +1000

    fix: updated indiview script

--- indiview.py
+++ indiview.py
@@ -1,26 +1,27 @@
-'''
-AutoTrader Indiview
---------------------
-A general script to view price data and indicators.
-'''
+from datetime import datetime
+from autotrader import AutoData, AutoPlot, indicators
 
-from autotrader.autodata import GetData
-from autotrader.autoplot import AutoPlot
-from autotrader import indicators
 
 # Instantiate GetData class
-get_data = GetData()
+get_data = AutoData(data_source="yahoo")
 
 # Get price data for EUR/USD
 instrument = 'EURUSD=X'
-data = get_data.yahoo(instrument, '1h', 
-                      start_time='2021-01-01', 
-                      end_time='2021-03-01')
+data = get_data.fetch(
+    instrument=instrument, 
+    granularity='1h', 
+    start_time=datetime(2021, 1, 1), 
+    end_time=datetime(2021, 3, 1),
+)
 
 # Construct indicators dictionary
 halftrend_df = indicators.halftrend(data)
-indicator_dict = {'HalfTrend': {'type': 'HalfTrend',
-                                'data': halftrend_df}}
+indicator_dict = {
+    'HalfTrend': {
+        'type': 'HalfTrend',
+        'data': halftrend_df
+    }
+}
 
 # Instantiate AutoPlot and plot
 ap = AutoPlot(data)
